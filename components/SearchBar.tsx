98244779b4aa2f8fe9d79d7e40e39fcfe9bec50c
sanderkg <sanderkvale@me.com>
2024-07-20 14:12:30 -0600

    Converted placeholder searchbar with input

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,9 +3,13 @@ import { LiaSearchSolid } from "react-icons/lia";
 
 const SearchBar = () => {
   return (
-    <div className='w-full flex items-center gap-5 '>
-        <LiaSearchSolid size={40} color='#cccccc'/>
-        <p className='text-2xl text-[#cccccc]'>Search through your Notes</p>
+    <div className='w-full flex items-center gap-5'>
+      <LiaSearchSolid size={40} color='#cccccc'/>
+      <input
+        type="text"
+        placeholder="Search through your Notes"
+        className='flex-1 py-2 px-4 border-b border-gray-300 bg-transparent focus:outline-none'
+      />
     </div>
   )
 }
