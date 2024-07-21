4a4597be581e6d7298486cf14696ddefaf0e228d
sanderkg <sanderkvale@me.com>
2024-07-20 18:33:16 -0600

    Decreased padding on note tiles

--- components/NoteTile.tsx
+++ components/NoteTile.tsx
@@ -5,9 +5,9 @@ import { PiPushPinThin } from "react-icons/pi";
 
 const NoteTile = () => {
     return (
-        <div className='w-full py-6 group grid grid-cols-8 max-w-screen-lg'>
+        <div className='w-full py-2 group grid grid-cols-8 max-w-screen-lg'>
             <div className='opacity-0 group-hover:opacity-100 transition-opacity duration-300 col col-span-1 flex items-center justify-center'>
-                < PiPushPinThin size={40} />
+                < PiPushPinThin size={40} color='#cccccc'/>
             </div>
             <div className='col col-span-6'>
                 <p className='text-3xl text-[#8d8d8d] mb-3 font-semibold'>Lorem ipsum dolor sit amet, consectetur.</p>
