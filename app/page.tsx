129440495bad6524f88ce2f2485fdfd0d54579ea
sanderkg <sanderkvale@me.com>
2024-07-20 20:46:44 -0600

    Updated style when list is empty and added funct

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
     <main className="min-h-screen flex justify-center md:p-24 p-6 bg-cover bg-[url('/Background.png')]">
       <div className="flex flex-col items-center w-full">
         <Header addNote={addNote} />
-        <Feed notes={notes}/>
+        <Feed notes={notes} addNote={addNote}/>
       </div>
     </main>
   );
--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,7 +1,9 @@
 "use client";
-import React from 'react';
+import React, { useState } from 'react';
 import NoteTile from './NoteTile';
 import SearchBar from './SearchBar';
+import { FaPlusCircle } from 'react-icons/fa';
+import AddNote from './AddNote';
 
 interface Note {
   id: string;
@@ -10,19 +12,47 @@ interface Note {
 
 interface FeedProps {
   notes: Note[];
+  addNote: (content: string) => void;
 }
 
-const Feed = ({ notes }: FeedProps) => (
-  <div className='w-full gap-10 flex flex-col items-center'>
-    <SearchBar />
-    {notes.length === 0 ? (
-      <p>No notes found</p>
-    ) : (
-      notes.map(note => (
-        <NoteTile key={note.id} note={note} />
-      ))
-    )}
-  </div>
-);
+const Feed = ({ notes, addNote }: FeedProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  return (
+    <div className='w-full gap-10 flex flex-col items-center'>
+      <SearchBar />
+      {notes.length === 0 ? (
+        <div className='w-full group grid grid-cols-8 max-w-screen-lg'>
+        <div className='col col-span-1'>
+        </div>
+        <div className='col col-span-6 w-full'>
+        <div className='flex flex-col w-full'>
+          <p className='text-2xl text-[#8d8d8d] mb-3'>Add your first note.</p>
+          <button onClick={openModal}>
+            <FaPlusCircle size={50} color="#61eb6e" />
+          </button>
+          {isModalOpen && (
+                <AddNote 
+                    closeModal={closeModal} 
+                    addNote={addNote}
+                />
+            )}
+        </div>
+        </div>
+        <div className='col col-span-1'></div>
+      </div>
+        
+
+      ) : (
+        notes.map(note => (
+          <NoteTile key={note.id} note={note} />
+        ))
+      )}
+    </div>
+  )
+}
 
 export default Feed;
